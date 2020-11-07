0054e3fdf92f08f8279811b7351eacfb26536f9c
RJ <whiteymccracker@mail.ru>
2020-11-06 17:41:09 -0700

    Add files via upload
    This should be the initial commit of my initial attempt for the script.

---
+++ winnypaste.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ask for a command to be URL pasted:
+read -p "Enter text file: " cmdvar
+
+
+
+
+
+# Here's winny's paste thingy. Appears to be more robust than wgetpaste -c
+alias urlpaste="curl -Ffile=@- https://0x0.st/ < $cmdvar"
+# alias finalurl="$urlpaste < $cmdvar"
+echo "Here's the URL: $urlpaste"
